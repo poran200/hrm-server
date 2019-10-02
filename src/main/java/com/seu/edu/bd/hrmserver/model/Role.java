72452e7e80e64a22213c24915808e1be6ce44bfa
poran200 <44917375+poran200@users.noreply.github.com>
2019-10-02 19:33:21 +0600

    Update Role.java

--- src/main/java/com/seu/edu/bd/hrmserver/model/Role.java
+++ src/main/java/com/seu/edu/bd/hrmserver/model/Role.java
@@ -1,9 +1,12 @@
 package com.seu.edu.bd.hrmserver.model;
 
 public enum Role {
-    DEPUTY_REGISTRAR,
+    DEPUTY_REGISTRAR_Academic,
     ADMISSION_OFFICER,
     EXAM_OFFICER,
     COORDINATOR,
-    HR_DEPUTY_REGISTRAR
+    DEPUTY_REGISTRAR_HRM,
+    STUDENT,
+    Faculty,
+    NO_ROLE
 }
